038990857afb1f6555e330537ee73df20e227fe2
Oliver-Strange <preston.burton@gmail.com>
2019-09-25 11:40:10 -0700

    sets up basic react app for mud project

---
+++ mudclient/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { Route } from "react-router-dom";
+import { connect } from "react-redux";
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Route exact path="/" component={} />
+        <Route path='/login' component={} />
+        <PrivateRoute path='/pyWar' component={} />
+      </div>
+    )
+  }
+}
+
+
+export default App;
