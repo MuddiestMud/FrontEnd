9107f304dd84425dd554a8bcdae615c3cbbbbfff
droneal11015 <droneal11015@gmail.com>
2019-09-26 20:35:39 -0400

    updated background component

--- mudclient/src/components/ImageComponents/Background.js
+++ mudclient/src/components/ImageComponents/Background.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import background from '../../design files/background.jpg'
+//import LoginButton from '../LoginComponents/Login'
 
 const BackgroundImage = () => {
     return (
         <div>
             <img src={background} alt="Background Image" className="background"/>
+           
         </div>
     );
 }
